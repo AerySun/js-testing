da64c7c0218af21b294281d4d2341a42618d1877
AerySun <janinequinten@gmail.com>
2021-05-12 13:03:17 +0200

    Variablen

---
+++ variable.js
@@ -0,0 +1,55 @@
+//Erstellen und Initialisieren einer Konstante
+const number = 10
+
+//Ausgabe der Konstante
+console.log(number)
+
+//Erstellen einer variablen mit dem Namen variable
+let variable = 5
+
+//Ausgabe der variablen
+console.log(variable)
+
+//Änderung des Wertes der Variablen variable auf 22
+variable = 22
+
+//Ausgabe der Variablen
+console.log(variable)
+
+//Definition und Zuweisung einer Zeichenfolge
+let string = 'A string'
+string = 'This is a string'
+
+//Ausgabe der Zeichenfolge
+console.log(string)
+
+//Definition und Zuweisung eines negativen Wertes
+let negative = -22
+
+//Ausgabe des negativen Wertes
+console.log(negative)
+
+//Definition und Zuweisung eines Wertes mit Nachkommastellen
+let num = 3.56
+
+console.log(num)
+
+//Definition mehrerer Variablen
+let a, b, c, d
+a = 5
+console.log(a) //Ausgabe 5
+
+//exercise 
+const number1 = 5
+let string1 = '10'
+console.log(number1, string1)
+string1 = 4
+console.log(string1)
+
+//exercise 2
+let helloString = 'Hallo Variablen'
+console.log(helloString, helloString, helloString)
+//i=index 1 zeichen variable
+for (let i = 0; i < 3; i++) { console.log(helloString) }
+
+
