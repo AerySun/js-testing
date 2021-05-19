3d848d2decc5cb35f181d087fece20c72b47193b
AerySun <janinequinten@gmail.com>
2021-05-19 19:05:20 +0200

    Rework

--- js-testing.js
+++ js-testing.js
@@ -2,3 +2,14 @@
 
 console.log('Hello World')
 console.log('Welcome in javascript')
+
+
+
+function addition (val1, val2, callback) {
+    callback(val1 + val2)
+}
+ function show(val) {
+     console.log(val)
+ }
+
+ addition (10, 5, show)
