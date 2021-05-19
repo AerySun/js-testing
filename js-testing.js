315607583fb2256db70a9aea52272ed36d62a48d
AerySun <janinequinten@gmail.com>
2021-05-19 19:09:28 +0200

    Reee

--- js-testing.js
+++ js-testing.js
@@ -13,3 +13,15 @@ function addition (val1, val2, callback) {
  }
 
  addition (10, 5, show)
+
+ function addition (val1, val2, callback) {
+     callback(val1)
+     callback(val2)
+     callback(val1 + val2)
+ }
+
+ function show(val) {
+    console.log(val)
+}
+
+addition (10, 5, show)
