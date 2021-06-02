aa6aa17a5ec444375c279bd4b7feb0739703143b
AerySun <janinequinten@gmail.com>
2021-06-02 11:06:30 +0200

    If, else exercise

--- ifelseswitch.js
+++ ifelseswitch.js
@@ -41,14 +41,67 @@ const daytime = 'morning'
 const greeting = daytime === 'morning' ? 'good morning' : 'good evening'
 console.log(greeting) 
 
-switch (available) {
-    case value:
-    //codeblock
-    break
-    case value2:
-    //codeblock
-    break
-    default: 
-    //codeblock
-    break
+// switch (available) {
+//     case value:
+//     //codeblock
+//     break
+//     case value2:
+//     //codeblock
+//     break
+//     default: 
+//     //codeblock
+//     break
+// }
+
+function greetings(time){
+    switch(time) {
+        case 'morning':
+        console.log('Good morning!')
+        break
+
+        case 'afternoon':
+        console.log('Good afternoon!')
+        break
+
+        case 'evening':
+        console.log('Good evening')
+        break
+
+        default:
+        console.log('Hello!')
+        break
+    }
 }
+greetings()
+
+
+function uebung1(val1, val2){
+    const result = val1 - val2
+
+    if(result > 0){
+        console.log(`The result ${result} is positive`)
+    } else {
+        console.log(`The result ${result} is negative`)
+    }
+}
+uebung1(10,5)
+uebung1(5,10)
+
+function uebung2(val){
+if ((val % 2)  === 0) {
+    console.log(`${val} ist teilbar durch 2`)
+} 
+    if((val % 5) === 0) {
+    console.log(`${val} ist durch 5 teilbar`)
+    }
+}
+uebung2(1)
+uebung2(2)
+uebung2(3)
+uebung2(4)
+uebung2(5)
+uebung2(6)
+uebung2(7)
+uebung2(8)
+uebung2(9)
+uebung2(10)
