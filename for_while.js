b5f460af3c1f39530902b48e3c49c2884443be2f
AerySun <janinequinten@gmail.com>
2021-06-15 15:57:42 +0200

    For and while loop

---
+++ for_while.js
@@ -0,0 +1,15 @@
+function forloop() {
+    for(let i = 0 ; i < 9 ; i++){
+        console.log(`The Number is ${i}`)
+    }
+}
+forloop()
+
+function whileloop(){
+    let i = 0
+    while(i < 9){
+        console.log(`The Number is ${i}`)
+        i += 1
+    }
+}
+whileloop()
