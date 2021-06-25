bc556a6ddf8fd28c1643375dc11ce87013722498
AerySun <janinequinten@gmail.com>
2021-06-26 00:45:13 +0200

    Own/game

---
+++ zgame_self.js
@@ -0,0 +1,19 @@
+//Simuliere ein Wuerfelspiel mit einem 8 Seitigen Wuerfel. Der Wuerfel wird so lange geworfen bis eine Zahl groesser gleich 5 herauskommt.
+// Dann werden alle Wuerfe zusammengezaehlt und plus 1 addiert.
+
+function roll_dice() {
+    return Math.floor(Math.random() * 8) + 1
+
+}
+
+let sum = 0
+
+while(true) {
+    const roll = roll_dice()
+    console.log(roll)
+    sum += roll
+    if(roll >= 5) break
+}
+
+let result = Math.floor(sum + 1)
+console.log(result)
