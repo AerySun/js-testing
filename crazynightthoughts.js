22bf5fdf7ef9fddb336d024762176406146d5459
AerySun <janinequinten@gmail.com>
2021-07-02 01:15:57 +0200

    More_night_thoughts

--- crazynightthoughts.js
+++ crazynightthoughts.js
@@ -15,3 +15,20 @@ while (last_roll < 8) {
     last_roll = roll
 }
 console.log(arr[arr.length - 1] + 2)
+
+//now instead of add 2, subtrahise the last roll through all rolls
+
+function roll_dice2() {
+    return Math.floor(Math.random() * 10) + 1
+}
+
+const arri = []
+let last_rolli = 0
+
+while (last_rolli < 8) {
+    const roll = roll_dice2()
+    console.log(roll)
+    arri.push(roll)
+    last_rolli = roll
+}
+console.log(arri[arri.length - 1] - arri.length)
